59909bcd6877595b9b3b58e0d0251574a08601e3
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2024-02-06 09:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenu = document.querySelector('.ham');
 const navLinks = document.querySelector('.nav-links');
 
 hamburgerMenu.addEventListener('click', () => {
